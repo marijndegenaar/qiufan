b63636c4235a56607aadc58d31433ecef5a0916d
Marijn Degenaar <marijndegenaar@gmail.com>
2025-11-03 11:16:22 +0100

    ha

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <template lang="pug">
+  p hello
   //- IndexList(contentType="project")
-p fuck
-.fixed.bottom-0.z-10
-  //- Gallery(:gallery="homepageData.data.gallery" :featured-image="homepageData.data.featuredImage" :grid="homepageData.data.show_as_grid" :autoscroll="homepageData.data.enable_autoscroll" :aspectRatio="'3/4'")
-  //- Slider(:gallery="homepageData.data.gallery" :items_to_show="3.5")
+
 </template>
 
 <script setup>
-// const { client } = usePrismic()
-// // get content from 'homepage'
-// const { data: homepageData } = await useAsyncData("homepage", () =>
-//   client.getSingle("homepage")
+
 // );
 </script>
 
